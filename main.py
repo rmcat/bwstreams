c8c393c7d4097fea45561fa48bfa573bfd94a88a
rmcat <rmcat@users.noreply.github.com>
2024-11-15 00:56:38 +1100

    add flask and gunicorn

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='www', static_url_path='')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
